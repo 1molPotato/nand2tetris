8da91bc9b82fd379fa133afce820db3daab2326e
lyu.kaiyuan <lyu.kaiyuan@gmail.com>
2018-12-22 16:34:10 +0800

    Implemented Fill functionality using HACK assembly language.

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,84 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+// Pseudo Code
+//  BLACKEN
+//  INFLOOP
+//  if KBD = 0
+//      if SCREEN[0] = 1
+//          write white in every pixel
+//      goto INFLOOP
+//  else
+//      write black in every pixel
+//      goto INFLOOP
+
+    @8192
+    D=A
+    @n
+    M=D // n = 8192
+
+(INFLOOP)
+    // reset i and addr
+    @i
+    M=0 // i = 0
+    @SCREEN
+    D=A
+    @addr
+    M=D // addr = SCREEN
+    @KBD
+    D=M
+    @WHITEN
+    D;JEQ // if RAM[KBD] = 0 goto WHITEN
+// else BLACKEN the screen
+(BLACEN)
+    @SCREEN
+    D=M+1
+    @INFLOOP
+    D;JEQ // if RAM[SCREEN] = -1 goto INFLOOP (no need to write black)
+
+(BLACKENLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @INFLOOP
+    D;JGE // if i >= n goto INFLOOP
+    @addr
+    A=M
+    M=-1 // write black to 16 pixels in addr
+    @addr
+    M=M+1 // addr = addr + 1
+    @i
+    M=M+1
+    @BLACKENLOOP
+    0;JMP // goto WHITENLOOP
+
+(WHITEN)
+    @SCREEN
+    D=M
+    @INFLOOP 
+    D;JEQ // if RAM[SCREEN] = 0 goto INFLOOP (no need to write white)
+
+(WHITENLOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @INFLOOP
+    D;JGE // if i >= n goto INFLOOP
+    @addr
+    A=M
+    M=0 // write white to 16 pixels in addr
+    @addr
+    M=M+1 // addr = addr + 1
+    @i
+    M=M+1
+    @WHITENLOOP
+    0;JMP // goto WHITENLOOP
+
+
+
+
+
+
+
